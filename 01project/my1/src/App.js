a47ffc142c1d34ff73d7b6078c074cd8d4ed20b3
IDeichman <100754738+IDeichman@users.noreply.github.com>
2022-03-24 15:20:28 +0300

    добавил ссылки как вкладки

--- 01project/my1/src/App.js
+++ 01project/my1/src/App.js
@@ -3,10 +3,24 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-   Simple HTML
+    <div>
+  <div className="App">
+ AutoCult.by
+   <div><Header /></div>
    </div>
-  );
+</div>)
+}
+const Header = () => {
+  return(
+    <div>
+      <a href='#s'>    Cars     </a>
+      <a href='#s'>    Logbook    </a>
+      <a href='#s'>    Community    </a>
+      <a href='#s'>    Service    </a>
+      <a href='#s'>    Shop    </a>
+      <a href='#s'>    Sale    </a>
+    </div>
+  )
 }
 
 export default App;
