28ee6f694857ca749fb8a569e34bde29e0dc0deb
IDeichman <100754738+IDeichman@users.noreply.github.com>
2022-04-10 13:12:51 +0300

    добавил компоненту Профайл

--- 01project/my1/src/App.js
+++ 01project/my1/src/App.js
@@ -1,7 +1,8 @@
- import React from 'react';
+import React from 'react';
 import './App.css';
 import Header from './componenta/Header';
 import NavBar from './componenta/NavBar';
+import Profile from './componenta/Profile';
 
 
 const App = () => {
@@ -9,32 +10,7 @@ const App = () => {
     <div className='app-wrapper'>
 <Header/>
 <NavBar/>
-  <div className='content'>
-    <div>
- <img src='https://komandirovka.ru/upload/iblock/a42/a426f0dea1d58256260cc50f4088c84c.jpg'/>
-  </div>
-  <div>
-    ava+description
-  </div>
-  <div>
-    <div>
-    my posts
-    </div>
-    <div>
-      new post
-    </div>
-    </div>
-    <div>
-      <div>
-      post1
-    </div>
-    <div>
-      post2
-    </div>
-  </div>
-  </div>
-
-
+<Profile/>
 </div>
   )
 }
---
+++ 01project/my1/src/componenta/Profile.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Profile = () =>{
+    return <div className='content'>
+    <div>
+ <img src='https://komandirovka.ru/upload/iblock/a42/a426f0dea1d58256260cc50f4088c84c.jpg'/>
+  </div>
+  <div>
+    ava+description
+  </div>
+  <div>
+    <div>
+    my posts
+    </div>
+    <div>
+      new post
+    </div>
+    </div>
+    <div>
+      <div>
+      post1
+    </div>
+    <div>
+      post2
+    </div>
+  </div>
+  </div>
+}
+
+export default Profile;
