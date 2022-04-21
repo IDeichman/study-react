40ef5b59e9ab1ee4bcf67ee5db8359e533331892
IDeichman <100754738+IDeichman@users.noreply.github.com>
2022-04-22 01:09:00 +0300

    20
    ошибку так и не исправил по цвету активной ссылки

--- 01project/my1/src/componenta/NavBar/NavBar.jsx
+++ 01project/my1/src/componenta/NavBar/NavBar.jsx
@@ -3,28 +3,30 @@ import { NavLink } from "react-router-dom";
 import c from './NavBar.module.css';
 
 const NavBar = () => {
-  return <nav className={c.nav}>
-    <div className={c.item}>
-      <h1>
-        <NavLink to="/profile" activeClassName={c.active}> Profile </NavLink>
-      </h1>
-    </div>
-    <div>
-      <h1 className={c.item}>
-        <NavLink to='/dialogs' activeClassName={c.active}> Messages </NavLink>
-      </h1>
-    </div>
-    <div className={c.item}>
-      <h1>
-        <NavLink to='/news'>News</NavLink>
-      </h1>
-    </div>
-    <div className={c.item}>
-      <h1>
-        <NavLink to='/settings'> Settings </NavLink>
-      </h1>
-    </div>
-  </nav>
+  return (
+    <nav className={c.nav}>
+      <div className={c.item}>
+        <h1>
+          <NavLink to="/profile" activeClassName={c.activeLink}> Profile </NavLink>
+        </h1>
+      </div>
+      <div>
+        <h1 className={c.item}>
+          <NavLink to='/dialogs' activeClassName={c.activeLink} > Messages </NavLink>
+        </h1>
+      </div>
+      <div className={c.item}>
+        <h1>
+          <NavLink to='/news' activeClassName={c.activeLink}> News </NavLink>
+        </h1>
+      </div>
+      <div className={c.item}>
+        <h1>
+          <NavLink to='/settings' activeClassName={c.activeLink}> Settings </NavLink>
+        </h1>
+      </div>
+    </nav>
+  )
 }
 
 export default NavBar;
