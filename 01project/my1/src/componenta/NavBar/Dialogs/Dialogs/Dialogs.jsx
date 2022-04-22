345320ac94f78a36e51d007ea9be9f8d81eb9aa0
IDeichman <100754738+IDeichman@users.noreply.github.com>
2022-04-22 13:12:05 +0300

    21 Dialog
    верстка компоненты диалог

--- 01project/my1/src/componenta/NavBar/Dialogs/Dialogs/Dialogs.jsx
+++ 01project/my1/src/componenta/NavBar/Dialogs/Dialogs/Dialogs.jsx
@@ -1,13 +1,34 @@
 import React from "react";
+import c from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            <h2>
-                Dialogs
-            </h2>
-        </div>
-    );
-}
+        <div className={c.dialogs}>
+            <div className={c.dialogItems}>
+                <div className={c.dialog}>
+                    Natallia
+                </div>
+                <div className={c.dialog}>
+                    Alesya
+                </div>
+                <div className={c.dialog}>
+                    Vika
+                </div>
+            </div>
 
+            <div className={c.messages}>
+                <div className={c.message}>
+                    Hi
+                </div>
+                <div className={c.message}>
+                    Hello
+                </div>
+                <div className={c.message}>
+                    i'm here
+                </div>
+            </div>
+        </div>
+
+    )
+}
 export default Dialogs;
